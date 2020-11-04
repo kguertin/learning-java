78d9d3542f856ee48bf1cb3205b989ef7f550e51
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-04 12:56:01 -0500

    java data types int, long, short, byte

---
+++ ByteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myValue = 10000;
+	    int myMinIntValue= Integer.MIN_VALUE;
+	    int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted Max Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Min Value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2147483647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Min Value = " + myMinByteValue);
+        System.out.println("Byte Max Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Min Value = " + myMinShortValue);
+        System.out.println("Short Max Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Min Value = " + myMinLongValue);
+        System.out.println("Long Max Value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2147483648L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+        
+    }
+}
