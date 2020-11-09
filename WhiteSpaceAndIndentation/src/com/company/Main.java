e7c3a0fabafd1ac777082148f691c0b0948dadb9
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-09 10:46:35 -0500

    if else statements in java

---
+++ WhiteSpaceAndIndentation/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myVariable = 50;
+	    myVariable++;
+	    // Can span many lines as long as there is not a terminator ex. ;
+        System.out.println("This is " +
+                "A test " +
+                "Over many lines");
+
+        // can have multiple statements on the same line.
+        int anotherVar = 10; int aThirdVar = 3;
+
+
+    }
+}
