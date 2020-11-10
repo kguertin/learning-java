f92c58b631d0ab0db080ce2dedb9c68655b0e99c
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-10 10:59:40 -0500

    java method challange

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,9 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your final score is " + highScore);
 
-        calculateScore(true, 10000, 8, 2000);
+
+        highScore = calculateScore(true, 10000, 8, 2000);
+        System.out.println("Your final score is " + highScore);
+
+        int position = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tom", position);
+
+        position = calculateHighScorePosition(900);
+        displayHighScorePosition("Ann", position);
+
+        position = calculateHighScorePosition(400);
+        displayHighScorePosition("Mark", position);
+
+        position = calculateHighScorePosition(50);
+        displayHighScorePosition("Bort", position);
+
+        position = calculateHighScorePosition(1000);
+        displayHighScorePosition("Sally", position);
 
     }
 
@@ -15,10 +33,25 @@ public class Main {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score is " + finalScore);
             return finalScore;
         }
 
         return -1;
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000){
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500){
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+    public static void displayHighScorePosition (String name, int position) {
+        System.out.println(name + " managed to get into position " + position + " on the high score table");
     }
 }
