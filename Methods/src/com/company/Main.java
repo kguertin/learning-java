c73ef728b67edfc71b638bf18e807d8b37b67d72
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-10 11:17:45 -0500

    refactor method challenge

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -40,18 +40,30 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        }
+//
+//        return 4;
+
+        int position = 4;
+
         if (playerScore >= 1000) {
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000){
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500){
-            return 3;
-        } else {
-            return 4;
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
         }
+
+        return position;
     }
 
-    public static void displayHighScorePosition (String name, int position) {
+    public static void displayHighScorePosition(String name, int position) {
         System.out.println(name + " managed to get into position " + position + " on the high score table");
     }
 }
