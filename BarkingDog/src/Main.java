b1a751d7422710a1855096acaebf5666b0a11468
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-17 00:33:08 -0500

    barking dog java kata

---
+++ BarkingDog/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(BarkingDog.shouldWakeUp(true, 23));
+        System.out.println(BarkingDog.shouldWakeUp(false, 6));
+        System.out.println(BarkingDog.shouldWakeUp(true, 5));
+        System.out.println(BarkingDog.shouldWakeUp(true, 30));
+    }
+}
