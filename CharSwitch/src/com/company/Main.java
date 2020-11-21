c893d21de4a69236e95e5454ed5fecb0725398bf
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-21 12:12:57 -0500

    Java char switch

---
+++ CharSwitch/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char letter = 'E';
+
+        switch (letter) {
+            case 'A':
+                System.out.println("Found Letter A");
+                break;
+            case 'B':
+                System.out.println("Found Letter B");
+                break;
+            case 'C':
+                System.out.println("Found Letter C");
+                break;
+            case 'D':
+                System.out.println("Found Letter D");
+                break;
+            case 'E':
+                System.out.println("Found Letter E");
+                break;
+            default:
+                System.out.println("Letter Not Found");
+        }
+
+    }
+}
