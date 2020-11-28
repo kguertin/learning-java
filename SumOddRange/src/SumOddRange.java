4b27006fbf295566c68ef1eed1016c7d6e7f31a0
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-11-27 23:15:41 -0500

    sum odd java kata

---
+++ SumOddRange/src/SumOddRange.java
@@ -0,0 +1,26 @@
+public class SumOddRange {
+
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+        if (end < start || end < 0 || start < 0) {
+            return -1;
+        }
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                sum += i;
+            }
+
+        }
+        return sum;
+    }
+
+    public static boolean isOdd(int number) {
+        if (number < 0) {
+            return false;
+        }
+        if (number % 2 == 0) {
+            return false;
+        }
+        return true;
+    }
+}
