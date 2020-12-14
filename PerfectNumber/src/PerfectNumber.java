dc9a629e10b95055d5166a999abf1f1b28afa47a
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-12-14 10:50:58 -0500

    perfect number java kata

---
+++ PerfectNumber/src/PerfectNumber.java
@@ -0,0 +1,15 @@
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number){
+        if(number < 1){
+            return false;
+        }
+        int total = 0;
+        for(int i = 1; i < number; i++){
+
+            if(number % i == 0) {
+                total += i;
+            }
+        }
+        return total == number;
+    }
+}
